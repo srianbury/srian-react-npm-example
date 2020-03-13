dc35605efdfa7a51b83091874f58d772961ed755
Brian Sunbury <bsunbury29@gmail.com>
2020-03-13 15:31:34 -0700

    removed css from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,6 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
-      },
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
       }
     ]
   },
