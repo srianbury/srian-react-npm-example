85314f7ac8ce7f10e974b29ed80ac13f27aaa315
Brian Sunbury <bsunbury29@gmail.com>
2020-03-13 15:29:00 -0700

    rm style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
       {
         test: /\.css$/,
         loader: "style-loader!css-loader"
